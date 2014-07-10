26744e754d459aab109a963320d37b0c2ec17703
Liso Gallo <liso@riseup.net>
2014-07-10 12:25:10 -0300

    [IMP] kinesis_athletics_view_mods code cleaned up

--- addons/kinesis_athletics_view_mods/__openerp__.py
+++ addons/kinesis_athletics_view_mods/__openerp__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
-{   'active': False,
+{
+   'active': False,
     'name': 'Kinesis View Modifications',
-    'author': 'Ingenieria Adhoc',
+    'author': 'Ingeniería Adhoc',
     'description': 'Kinesis View Modifications',
     'website': 'www.ingadhoc.com',
     'images': [
     ],
     'category': 'base.module_category_hidden',
-    'demo_xml': [
-      ],
+    'demo': [],
     'depends': ['kinesis_athletics_x'],
-    'init_xml': [],
+    'init': [],
     'installable': True,
     'license': 'AGPL-3',
     'test': [],
-    'update_xml': [
-                  u'view/menuitem.xml',
-                  u'view/partner_view.xml',
-                      ],
+    'data': [
+        u'view/menuitem.xml',
+        u'view/partner_view.xml',
+        ],
     'version':'1.0'
-    }
+}
