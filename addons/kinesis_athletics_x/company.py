d620d18ff0eb94c004fef08565338b84d43ebc2b
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-12-10 10:42:11 -0300

    Clean code

--- addons/kinesis_athletics_x/company.py
+++ addons/kinesis_athletics_x/company.py
@@ -1,26 +1,11 @@
 # -*- coding: utf-8 -*-
+from openerp import models, fields
 
-from openerp import models, fields, api, _
 
 class company(models.Model):
     """"""
 
-    _name = 'res.company'
     _inherit = 'res.company'
 
-    groups_ids = fields.One2many('kinesis_athletics.group', 'company_id', string='Groups')
-    active = fields.Boolean(related='partner_id.active', string="Active")
-
-    def _check_groups(self, cr, uid, ids, context=None):
-        obj = self.browse(cr, uid, ids[0], context=context)
-
-        if obj.use_groups !=True:
-            if obj.groups_ids:
-                return False
-
-        return True
-
-    _constraints = [(_check_groups, 'Tiene cursos/areas cargadas', ['groups_ids'])
-    ]
-
-company()
+    groups_ids = fields.One2many(
+        'kinesis_athletics.group', 'company_id', string='Groups')
--- addons/kinesis_athletics_x/evaluation.py
+++ addons/kinesis_athletics_x/evaluation.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
-
-from openerp import netsvc
-from openerp.osv import osv, fields
 from openerp import models, api
-from openerp import fields as fields_new
+from openerp import fields as fields
 from openerp.tools.translate import _
 from datetime import datetime
-from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, DATETIME_FORMATS_MAP, float_compare
 
 
 class evaluation(models.Model):
+
     """"""
 
     _inherit = 'kinesis_athletics.evaluation'
@@ -18,7 +15,8 @@ class evaluation(models.Model):
     @api.one
     @api.depends('template_id', 'name')
     def _complete_name(self):
-        """ Forms complete name of location from parent location to child location.
+        """ Forms complete name of location from parent location to child
+        location.
         @return: Dictionary of values
         """
         if self.template_id.name:
@@ -30,42 +28,48 @@ class evaluation(models.Model):
     @api.depends('company_id', 'date')
     def _calc_date(self):
         if self.date:
-            eval_year_id=(datetime.strptime(self.date, '%Y-%m-%d')).date()
-            self.eval_year= eval_year_id.year
+            eval_year_id = (datetime.strptime(self.date, '%Y-%m-%d')).date()
+            self.eval_year = eval_year_id.year
         else:
             self.eval_year = False
-        
 
-    eval_year = fields_new.Integer(
+    @api.one
+    def _get_partner_age(self):
+        self.age = self.partner_id.age
+
+    eval_year = fields.Integer(
         compute='_calc_date',
         string="Year",
         store=True)
-    complete_name = fields_new.Char(
+    complete_name = fields.Char(
         compute='_complete_name',
         string="Name",
         store=True)
-
-    def _get_partner_age(self, cr, uid, ids, name, arg, context=None):
-        res = {}
-
-        for record in self.browse(cr, uid, ids, context=context):
-            res[record.id] = record.partner_id.age
-
-        return res
-
-
-    _columns = {
-        'age': fields.function(_get_partner_age, type='integer', string="Age", store=True),
-        'evaluation_detail_value_ids': fields.one2many('kinesis_athletics.evaluation_detail', 'evaluation_id', string='Values', domain=[('test_type','=','value')], readonly=True),
-        'evaluation_detail_selection_ids': fields.one2many('kinesis_athletics.evaluation_detail', 'evaluation_id', string='Selections', domain=[('test_type','=','selection')], readonly=True),
-        'use_groups':fields.related('company_id','use_groups',relation='res.company', type='boolean', string='Use Group', store=True),
-        #we overwrite this field because in v8 o2m fields has copy=False by default and copy is not implemented on xmi2oerp
-        'evaluation_detail_ids': fields.one2many('kinesis_athletics.evaluation_detail', 'evaluation_id', string='Evaluation Details', required=True, copy=True),
-    }
+    age = fields.Integer(
+        compute='_get_partner_age',
+        string="Age",
+        store=True)
+    evaluation_detail_value_ids = fields.One2many(
+        'kinesis_athletics.evaluation_detail',
+        'evaluation_id',
+        string='Values',
+        domain=[('test_type', '=', 'value')],
+        readonly=True)
+    evaluation_detail_selection_ids = fields.One2many(
+        'kinesis_athletics.evaluation_detail',
+        'evaluation_id',
+        string='Selections',
+        domain=[('test_type', '=', 'selection')],
+        readonly=True)
+    use_groups = fields.Boolean(
+        related='company_id.use_groups',
+        string='Use Group',
+        store=True)
+    evaluation_detail_ids = fields.One2many(copy=True)
+    # we overwrite this field because in v8 o2m fields has copy=False by
+    # default and copy is not implemented on xmi2oerp
 
     _order = 'date desc'
-
-
 
     def copy(self, cr, uid, id, default, context=None):
         if context is None:
@@ -77,9 +81,9 @@ class evaluation(models.Model):
 
         if not default.get("partner_id", False):
             default['partner_id'] = False
-        res = super(evaluation, self).copy(cr, uid, id, default=default, context=None)
+        res = super(evaluation, self).copy(
+            cr, uid, id, default=default, context=None)
         return res
-
 
     def on_change_group(self, cr, uid, ids, group_id, context=None):
         v = {}
@@ -93,7 +97,7 @@ class evaluation(models.Model):
 
             for partner in group.partner_ids:
                 partner_list.append(partner.id)
-            domain_dic['partner_id'] = [('id','in',partner_list)]
+            domain_dic['partner_id'] = [('id', 'in', partner_list)]
 
         v['partner_id'] = False
 
@@ -101,7 +105,6 @@ class evaluation(models.Model):
             'value': v,
             'domain': domain_dic,
         }
-
 
     def on_change_company(self, cr, uid, ids, company_id, context=None):
         v = {}
@@ -120,31 +123,29 @@ class evaluation(models.Model):
 
         }
 
-
     def new_evaluation(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
-        new_evaluations = []
 
         for i in ids:
-            context.update({'copy':True})
+            context.update({'copy': True})
             current_evaluation = self.browse(cr, uid, i, context=context)
             if current_evaluation.is_template:
-              new_id = self.copy(cr, uid, i, default = {
-                                                  'date': datetime.today(),
-                                                  'template_id':i,
-                                                  'is_template':False,
-                                                  }, context=context)
+                new_id = self.copy(cr, uid, i, default={
+                    'date': datetime.today(),
+                    'template_id': i,
+                    'is_template': False,
+                }, context=context)
             else:
-              new_id = self.copy(cr, uid, i, default = {
-                                'date': datetime.today(),
-                                'partner_id': current_evaluation.partner_id.id,
-                                'group_id': current_evaluation.partner_id.actual_group_id.id,
-                                  }, context=context)
+                new_id = self.copy(cr, uid, i, default={
+                    'date': datetime.today(),
+                    'partner_id': current_evaluation.partner_id.id,
+                    'group_id': current_evaluation.partner_id.actual_group_id.id,
+                }, context=context)
 
-              for evaluation_detail in self.browse(cr, uid, new_id, context=context).evaluation_detail_ids:
-                evaluation_detail.write({'result': False})
-                evaluation_detail.write({'test_selection_id': False})
+                for evaluation_detail in self.browse(cr, uid, new_id, context=context).evaluation_detail_ids:
+                    evaluation_detail.write({'result': False})
+                    evaluation_detail.write({'test_selection_id': False})
 
         return {
             'name': _('Evaluation'),
@@ -156,7 +157,6 @@ class evaluation(models.Model):
             'type': 'ir.actions.act_window',
             'nodestroy': True
         }
-
 
     def _check_person(self, cr, uid, ids, context=None):
         obj = self.browse(cr, uid, ids[0], context=context)
